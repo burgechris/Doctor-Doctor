390756463c800df3b7c7fca0814debb75f323534
Chris Burge <topher.burge@gmail.com>
2019-09-13 09:02:38 -0700

    add api call codeand some html structure.

---
+++ src/doctor.js
@@ -0,0 +1,17 @@
+export class BetterDoctor {
+  getDoctor(name, location, condition) {
+    return new Promise(function(resolve, reject) {
+      let request = new XMLHttpRequest();
+      const url = `https://api.betterdoctor.com/2016-03-01/doctors?`;
+      request.onload = function() {
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(Error(request.statusText));
+        }
+      }
+      request.open("GET", url, true);
+      request.send();
+    });
+  }
+}
---
+++ src/main.js
@@ -0,0 +1,10 @@
+mport $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles.css';
+import { BetterDoctor } from './doctor';
+
+$(document).ready(function() {
+
+
+});
