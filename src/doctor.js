65e82bcf4e786bdae52c30564d9b0096eae6ea20
Chris Burge <topher.burge@gmail.com>
2019-09-13 10:20:39 -0700

    parse json response to find first name.

--- src/doctor.js
+++ src/doctor.js
@@ -2,7 +2,7 @@ export class BetterDoctor {
   getDoctor(name, condition) {
     return new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
-      const url = `https://api.betterdoctor.com/2016-03-01/doctors?location=or-portland&user_key=${process.env.exports.apiKey}`;
+      const url = `https://api.betterdoctor.com/2016-03-01/doctors?location=or-portland&user_key=${process.env.exports.apiKey}&name=${name}&query=${condition}`;
       request.onload = function() {
         if (this.status === 200) {
           resolve(request.response);
--- src/main.js
+++ src/main.js
@@ -10,5 +10,16 @@ $(document).ready(function() {
     let name = $('#docName').val();
     let condition = $('#cond').val();
 
+
+    let docSearch = new BetterDoctor();
+    let promise = docSearch.getDoctor(name, condition);
+
+    promise.then(function(response) {
+      let body = JSON.parse(response);
+      console.log(body.data[0].profile.first_name);
+      $('.name').text(body.data[0].profile.first_name)
+    }, function(error) {
+      $('.error').text(`There was an error processing your request: ${error.message}`);
+    });
   });
 });
