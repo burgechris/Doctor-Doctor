902d8c9b7afa4c0ec71c38898d00b8f311625405
Chris Burge <topher.burge@gmail.com>
2019-09-13 16:47:52 -0700

    refactor code.

--- src/main.js
+++ src/main.js
@@ -16,20 +16,7 @@ $(document).ready(function() {
 
     promise.then(function(response) {
       const body = JSON.parse(response);
-      for (var i = 0; i < body.data.length; i++) {
-        let firstName = body.data[i].profile.first_name;
-        let lastName = body.data[i].profile.last_name;
-        let title = body.data[i].profile.title;
-        for (var j = 0; j < body.data[i].practices.length; j++) {
-          let street = body.data[i].practices[j].visit_address.street;
-          let city = body.data[i].practices[j].visit_address.city;
-          let state = body.data[i].practices[j].visit_address.state;
-          let zip = body.data[i].practices[j].visit_address.zip;
-          let phone = body.data[i].practices[j].phones.number;
-          // let website =
-        }
-
-        // $('.results').append(
+      // $('.results').append(
         //   `<div class=" results">
         //     <div class="card">
         //       <h5 class="name"></h5>
@@ -39,6 +26,20 @@ $(document).ready(function() {
         //       <div class="new"></div>
         //     </div>
         //   </div>`)
+    
+      for (var i = 0; i < body.data.length; i++) {
+        let firstName = body.data[i].profile.first_name;
+        let lastName = body.data[i].profile.last_name;
+        let title = body.data[i].profile.title;
+
+        for (var j = 0; j < body.data[i].practices.length; j++) {
+          let street = body.data[i].practices[j].visit_address.street;
+          let city = body.data[i].practices[j].visit_address.city;
+          let state = body.data[i].practices[j].visit_address.state;
+          let zip = body.data[i].practices[j].visit_address.zip;
+          let phone = body.data[i].practices[j].phones.number;
+        }
+
 
       }
 
