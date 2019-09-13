8ffdf66cbd22dbaf4ef969056ccbd5989c26caf3
Chris Burge <topher.burge@gmail.com>
2019-09-13 09:31:49 -0700

    add code to main.js and doctor.js.

--- src/doctor.js
+++ src/doctor.js
@@ -1,8 +1,8 @@
 export class BetterDoctor {
-  getDoctor(name, location, condition) {
+  getDoctor(name, condition) {
     return new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
-      const url = `https://api.betterdoctor.com/2016-03-01/doctors?`;
+      const url = `https://api.betterdoctor.com/2016-03-01/doctors?location=or-portland&user_key=${process.env.exports.apiKey}`;
       request.onload = function() {
         if (this.status === 200) {
           resolve(request.response);
--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
-mport $ from 'jquery';
+import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 import { BetterDoctor } from './doctor';
 
 $(document).ready(function() {
+  $('#doctorSearch').submit(function(event) {
+    event.preventDefault();
+    let name = $('#docName').val();
+    let condition = $('#cond').val();
 
-
+  });
 });
