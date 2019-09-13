22bb84759271e679dec31f2b159cea833354ce15
Chris Burge <topher.burge@gmail.com>
2019-09-13 12:57:21 -0700

    add loop to check for practice name, phone number, and new patients and then push them into separate arrays.

--- src/main.js
+++ src/main.js
@@ -14,15 +14,22 @@ $(document).ready(function() {
     let docSearch = new BetterDoctor();
     let promise = docSearch.getDoctor(name, condition);
 
-    let doctors = [];
-    console.log(doctors);
+    let docName = [];
+    let docAddress = [];
+    let docPhone = [];
+    let newPatients = [];
+    console.log(docName);
+    console.log(docPhone);
+    console.log(newPatients);
     promise.then(function(response) {
       let body = JSON.parse(response);
-      for (var i = 0; i < body.data.length; i++) {
-        doctors.push(body.data[i].practices)
-        // console.log(body.data[i].practices);
+      for (var i = 0; i < body.data[0].practices.length; i++) {
+        docName.push(body.data[0].practices[i].name)
+        docPhone.push(body.data[0].practices[i].phones[0])
+        newPatients.push(body.data[0].practices[i].accepts_new_patients)
+
       }
-      $('.firstName').append(body.data[0].practice[i].name)
+      $('.firstName').text(body.data[i])
     }, function(error) {
       $('.error').text(`There was an error processing your request: ${error.message}`);
     });
