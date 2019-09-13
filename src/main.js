2b9f5795aca9340044b35038c98f36b99a2bf4fe
Chris Burge <topher.burge@gmail.com>
2019-09-13 16:02:02 -0700

    store data in individual variables.

--- src/main.js
+++ src/main.js
@@ -17,18 +17,28 @@ $(document).ready(function() {
     promise.then(function(response) {
       const body = JSON.parse(response);
       for (var i = 0; i < body.data.length; i++) {
-        $('.results').append(
-          `<div class=" results">
-            <div class="card">
-              <h5 class="name"></h5>
-              <div class="address"></div>
-              <div class="phoneNumber"></div>
-              <div class="website"></div>
-              <div class="new"></div>
-            </div>
-          </div>`)
+        let firstName = body.data[i].profile.first_name;
+        let lastName = body.data[i].profile.last_name;
+        let title = body.data[i].profile.title;
+        for (var j = 0; j < body.data[i].practices.length; j++) {
+          let street = body.data[i].practices[j].visit_address.street;
+          let city = body.data[i].practices[j].visit_address.city;
+          let state = body.data[i].practices[j].visit_address.state;
+          let zip = body.data[i].practices[j].visit_address.zip;
+          let phone = body.data[i].practices[j].phones.number;
+          // let website =
+        }
 
-          $('.name').text(`${body.data[i].profile.first_name}`)
+        // $('.results').append(
+        //   `<div class=" results">
+        //     <div class="card">
+        //       <h5 class="name"></h5>
+        //       <div class="address"></div>
+        //       <div class="phoneNumber"></div>
+        //       <div class="website"></div>
+        //       <div class="new"></div>
+        //     </div>
+        //   </div>`)
 
       }
 
